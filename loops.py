1de75cf9e2ff3bba36febaaf2953b27d11960992
djibril soukouna <djib_cisse_9@hotmail.com>
2024-02-22 22:47:44 +0100

    loop for added

---
+++ loops.py
@@ -0,0 +1,14 @@
+# for in loop
+
+numbers = [1, 2, 3, 4, 5]
+# dog = { "name": ("Pitbull", "Dobermann"), "Dangerous": True, 93: "Yes", "numbers" : [1, 2, 3, 4, 5] }
+
+# for i in dog["numbers"]:
+#   print(i)
+
+sum = 0
+
+for x in range(10):
+  sum += x
+  print(sum)
+
