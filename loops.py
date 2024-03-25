8d4f03cdae0cb893f5ac282cf64c8151869ebd69
Djibril160 <134697508+Djibril160@users.noreply.github.com>
2024-03-25 12:07:32 +0100

    Update loops.py

--- loops.py
+++ loops.py
@@ -19,8 +19,14 @@ for x in range(10):
 
 rows = [ 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14]
 rows_even = rows[::2] 
+# [::2] utilise une opération de découpage de liste pour sélectionner chaque deuxième élément de la liste rows. Voici comment cela fonctionne :
+# rows : C'est la liste à partir de laquelle vous souhaitez sélectionner les éléments.
+# [::2] : C'est la syntaxe de découpage de liste.
+# Le premier : indique que vous allez spécifier le début et la fin de la tranche,
+# le deuxième : spécifie le pas, c'est-à-dire le nombre d'éléments à sauter entre chaque élément sélectionné.
 
 rows_odd = rows[1::2]
+# ici idem que là haut, sauf qu'on commence à l'index 1 
 
 for index, (row_even, row_odd) in enumerate(zip(rows_even, rows_odd)):
     if index >= 6:
